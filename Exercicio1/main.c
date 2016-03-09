21ae97bcf8bf324b42ac330eb5b95f42d3268be9
Caio Vinicius Piologo V Fernandes <ra145574@morais.lab.ic.unicamp.br>
2016-03-09 20:01:47 -0300

    codigo paralelisado corretamente

--- Exercicio1/main.c
+++ Exercicio1/main.c
@@ -9,31 +9,15 @@ main(int argc, char *argv[])
   n = atoi(argv[1]);
 
 /* Fork a team of threads giving them their own copies of variables */
-#pragma omp parallel private(tid, nthreads, parts) reduction(+:count)
-  {
+#pragma omp parallel
+{
 
-  /* Obtain thread number */
-  tid = omp_get_thread_num();
-
-  nthreads = omp_get_num_threads();
-  parts = n/nthreads;
-  //  Waits for the first thread to set the enviroment
-  // while(parts == 0){}
-  for (i = tid*parts; i < (tid+1)*parts; ++i){
-  	if(primo(i)){
-  		count++;
-  	}
-  }
-
-  /* Only master thread does this */
-  if (tid == nthreads - 1) 
-    {
-	   for (i = n - (n % nthreads); i <= n; ++i){
-	  	if(primo(i)){
-	  		count++;
-	  	}
-	   }   	
-    }
+	#pragma omp for reduction(+:count)
+		for (i=2; i < n; i++){
+			if(primo(i)){
+				count++;
+			}
+		}
   }  /* All threads join master thread and disband */
 
   printf("%d\n", count);
